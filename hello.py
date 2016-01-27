ee468d008704ccbdfbfeea335e145784a1fc93f8
cmis <cmis@cmis-hs10.(none)>
2016-01-27 11:14:02 +0700

    Saved my file

---
+++ hello.py
@@ -0,0 +1,14 @@
+print "Hello World!"
+print "I love you!"
+print "Maybe"
+print True
+print False
+print True
+print False
+print 1.1
+print "Do I know you?"
+print 44
+print ("Joo Ok", "Kim", "Inhye", "Baboo")
+print 33, 44, 55, 66, 77, 
+print "Right now" 
+print ("I am so hungry.", "I want to go home!") 
