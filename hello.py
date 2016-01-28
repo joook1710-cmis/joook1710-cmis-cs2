5ef98e8767398428dcafd1660e5a3d3189db06cb
cmis <cmis@cmis-hs10.(none)>
2016-01-28 14:34:26 +0700

    Saved the file

--- hello.py
+++ hello.py
@@ -12,3 +12,19 @@ print ("Joo Ok", "Kim", "Inhye", "Baboo")
 print 33, 44, 55, 66, 77, 
 print "Right now" 
 print ("I am so hungry.", "I want to go home!") 
+print "You and I"
+myName = "jooie kim"
+print 12
+x = 3*2
+x = 4*8 
+print x
+l = 3+1
+k = 3-1
+j = 8/4
+print l
+print k
+print j
+m = 6%5
+print m 
+m = 2%4
+print m 
