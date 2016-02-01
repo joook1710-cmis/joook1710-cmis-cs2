d6f17100583aed068779ae3de7bfed09b91ae4e3
cmis <cmis@cmis-hs10.(none)>
2016-02-01 09:55:59 +0700

    made python script

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+myName = "Joo Ok Kim" 
+print myName 
+myAge = 16.4
+print myAge
+myHeight = 1.65
+print myHeight
+lengthOfSquare = 4
+print lengthOfSquare
+lengthOfRectangle = 8
+print lengthOfRectangle
+heightOfRectangle = 16
+print heightOfRectangle
+monthInYears = 12
+myAgeInMonths = 12 * 16.4
+print myAgeInMonths
+yearsToCome = 80
+yearsToLive = myAge + yearsToCome  
+print yearsToLive
+myHeightInFeet = 5.4 
+print myHeightInFeet
+koreanFemaleHeight = 1.62
+differenceInHeight = myHeight - koreanFemaleHeight 
+print differenceInHeight
+areaOfSquare = 4 *4
+print areaOfSquare 
