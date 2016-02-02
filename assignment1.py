b7911082622b17f641aa6efbbe664a262656fbe7
cmis <cmis@cmis-hs10.(none)>
2016-02-02 11:56:59 +0700

    Edited a file

--- assignment1.py
+++ assignment1.py
@@ -1,25 +1,53 @@
+#Created a variable and defined my name. 
 myName = "Joo Ok Kim" 
 print myName 
+#Created a variable and defined my age. 
 myAge = 16.4
 print myAge
+#Created a variable and defined my height. 
 myHeight = 1.65
 print myHeight
+#Created a variable and defined the length of a sqaure.
 lengthOfSquare = 4
 print lengthOfSquare
+#Created a variable and defined the length of a rectangle. 
 lengthOfRectangle = 8
 print lengthOfRectangle
+#Created a variable and defined the height of the rectangle. 
 heightOfRectangle = 16
 print heightOfRectangle
+#Created a variable and defined my age in months. 
 monthInYears = 12
-myAgeInMonths = 12 * 16.4
+myAgeInMonths = monthInYears * myAge
 print myAgeInMonths
+#Created a variable and defined the years I would live. 
 yearsToCome = 80
 yearsToLive = myAge + yearsToCome  
 print yearsToLive
+#Created a variable and defined my height in feet. 
 myHeightInFeet = 5.4 
 print myHeightInFeet
+#Created a variable and defined the average height of a Korean. 
 koreanFemaleHeight = 1.62
 differenceInHeight = myHeight - koreanFemaleHeight 
 print differenceInHeight
+#Created a variable and defined the area of the square. 
 areaOfSquare = 4 *4
 print areaOfSquare 
+#Created a variable and defined half of the cube. 
+halfOfCube = 0.5 * 16 * 4 
+print halfOfCube
+#Created a variable and defined the area of the rectangle. 
+areaOfRectangle = lengthOfRectangle * heightOfRectangle 
+print areaOfRectangle 
+#Created a variable and defined one ninth of the area of the rectangle. 
+ninthAreaOfRectangle = 0.11 * areaOfRectangle 
+print ninthAreaOfRectangle 
+#Created a message. 
+print "Hello, it's me, " + myName + "." + " I am " + str(myAge) + " years old. " + " I am " + str(myHeightInFeet) + " feet tall. " + " I am " + str(myHeight) + " tall in meters. " + " Staticstically speaking, I can live up to " + str(yearsToLive) + " years. " 
+#Created another message. 
+print " My age in months is " , str(myAgeInMonths),"." , " Korean female's average height is " , str(koreanFemaleHeight) , " in meters." , " I like squares. The length of the square I created is " , str(lengthOfSquare), "." , " I like rectangles too. The length of the rectangle I created is " , str(lengthOfRectangle),"." , " The area of my rectangle is " , str(areaOfRectangle), "."
+#Created a varirable and printed the smiley faces. 
+smileyFace = ";)"
+print smileyFace * int(10000)
+
