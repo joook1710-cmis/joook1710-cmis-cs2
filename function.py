13f8a03d2056302b8d1c00fea5f1013dd7528f9c
cmis <cmis@cmis-hs10.(none)>
2016-02-09 14:06:46 +0700

    Saved File

---
+++ function.py
@@ -0,0 +1,41 @@
+#Added two arguements 
+def add(a,b):
+	return a + b 
+c = add(3, 4)
+print c
+
+#Subtraction two arguements 
+def sub(j,k):
+	return j - k
+l = sub(5, 3)
+print l 
+
+#Multiplied two arguements 
+def mul(r,t):
+	return r * t
+e = mul(4,4)
+print e
+
+#Divided two arguements
+def div(q,w):
+	return q / w
+y = div(2,3)
+print y 
+
+#Defined hours from seconds 
+def hours_from_seconds_div(a,b):
+	return a/b
+s = div(86400, 3600) 
+print s
+
+#Representation of a radius of a circle 
+def circle_area(r):
+	return 3.14159265359 * (r**2)
+print circle_area(5) 
+
+def sphere_volume(v):
+	return 3.14159265359 * 1.333333333333 * (v**3)
+print sphere_volume(5)
+
+def avg_volume(a,b):
+	return 
