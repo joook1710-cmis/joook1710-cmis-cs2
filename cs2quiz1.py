3fdb27c689ed032011bc53576d80c76209c70627
cmis <cmis@cmis-hs10.(none)>
2016-03-02 09:56:40 +0700

    Saved files

---
+++ cs2quiz1.py
@@ -0,0 +1,72 @@
+#Part 1: Terminology (15 points)
+#1 1pt) What is the symbol "=" used for?
+# It is used to define a function. 
+#
+#
+#2 3pts) Write a technical definition for 'function'
+# Function is a named sequence of statements that performs a computation. 
+# When you define a function, you specify the name and the sequence of statements. 
+#
+#3 1pt) What does the keyword "return" do?
+# When a function is called, the program stops and waits for the user to type something. When the user presses return, the program resumes and the output comes out. 
+#
+#
+#4 5pts) We know 5 basic data types. Write the name for each one and provide two
+#   examples of each below
+#	1: String
+# "Hello world" / "I love you" / str(44) 
+#	2: Tuple
+# ("Hello", "Yo", 44) / (1, 2, 3, 4)
+#	3: Boolean 
+# 4 < 5 / 2 = 1 
+#	4: Integer
+# 4 / int("hello")
+#	5: Floating point 
+# float(0.1) / float(3.7) 
+ 
+#5 2pts) What is the difference between a "function definition" and a 
+#        "function call"?
+# Function call operator is used for operations that require a number of parameters. This works because expression list is a list instead of a single operand. 
+# Programming languages come with a prewritten set of functions that are kept in a library. And function definition is used to define what is kept in the library. 
+#
+#
+#6 3pts) What are the 3 phases that every computer program has? What happens in
+#        each of them
+#	1: Input 
+# Collection refers to gathering the data from a variety of sources and assembling it. Verification means checking the data to determine whether it is accurate and complete. 
+#	2: Process
+# one or more of tasks may be performed on the input data. 
+#	3: Output 
+# Data are organized by characteristics meaninggul to the user. The user needs to call the function to bring out the output.
+ 
+#Part 2: Programming (25 points)
+#Write a program that asks the user for the areas of 3 circles.
+#It should then calculate the diameter of each and the sum of the diameters 
+#of the 3 circles.
+#Finally, it should produce output like this:
+
+import math 
+def int( e, x):
+	return e * x
+def output ( e, x, y):
+	return out 
+
+def main():
+	e= raw_input("c1: ")
+	x= raw_input("c2: ")
+	y= int(raw_input("c3: "))
+	
+main()
+
+
+
+
+#Circle  Diameter
+#c1      ...
+#c2      ...
+#c3      ...
+#TOTALS  ...
+
+# Hint: Radius is the square root of the area divided by pi
+
+
