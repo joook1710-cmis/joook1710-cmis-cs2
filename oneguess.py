c56ccda41daad4c8ef06e30c157a865e8de21a68
cmis <cmis@cmis-hs16.(none)>
2016-03-22 09:55:21 +0700

    saved files

--- oneguess.py
+++ oneguess.py
@@ -1,38 +1,51 @@
 import random
 import math
 
+def sub(a, b):
+	return int(a) - int(b)
+def add(c, d):
+	return int(c) + int(d)
+
+def guessing_small(chosen_number, estimation, difference):
+	guess_small = """
+The target was {}.
+Your guess was {}.
+That's under by {}.
+""".format(chosen_number, estimation, difference)
+	return guess_small
+
+def guessing_big(chosen_number, estimation, difference):
+	guess_big = """
+The target was {}.
+Your guess was {}.
+That's over by {}.
+""".format(chosen_number, estimation, difference)
+	return guess_big
+
+def guessing_correct(chosen_number, estimation):
+	guess_correct = """
+The target was {}.
+Your guess was {}.
+That's correct.
+""".format(chosen_number, estimation)
+	return guess_correct
 
 def main():
-	minimum= int(raw_input("What is the minimum number?: "))
-	maximum= int(raw_input("What is the maximum number?: "))
-
-	output = """ I'm thinking of a number between {} and {}. """.format (minimum, maximum)
-	print output 
-
-	number = random.randint(int(minimum), int(maximum))
-	your_number = raw_input("Guess the number:")
-	output = """ The number was {}: """.format(number)
-	print output
-	output = """ Your guess was {}: """.format(your_number)
-
-if your_number == number:
-	return True
-	output = " Congratulations, you guessed the number correctly"		
-	print output
-
-
-	if your_number > number:
-		difference = int(your_number) -  int(number)
-		
-	output = """ Thats {} higher than the number """.format(difference)
-	print output	
-	
-	if your_number < number:
-		more_than = int(number) - int(number)
-		
-	output = """ Your guess was under by {}  """.format(more_than)
-	print output 
-
+	#Input
+	minimum = int(raw_input("What is the minimum number? "))
+	maximum = int(raw_input("What is the maximum number? "))
+	estimation = raw_input("I'm thinking of a number between " + str(minimum) + " and " + str(maximum) + ".\n" + "What do you think it is?: ")
+	#Processing
+	chosen_number = int(random.randint(minimum, maximum))
+	difference = abs(sub(int(estimation), chosen_number))	
+	#Output
+	if chosen_number < abs(int(estimation)):	
+		guess_small = guessing_small(chosen_number, estimation, difference)
+		print guess_small
+	elif chosen_number > abs(int(estimation)):
+		guess_big = guessing_big(chosen_number, estimation, difference)
+		print guess_big
+	elif chosen_number == abs(int(estimation)):
+		guess_correct = guessing_correct(chosen_number, estimation)
+		print guess_correct
 main()
-
-
