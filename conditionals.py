7f949f29a9e2bf8690c49f3200fc78f0cd7225b6
cmis <cmis@cmis-hs16.(none)>
2016-03-23 11:51:45 +0700

    saved files

---
+++ conditionals.py
@@ -0,0 +1,3 @@
+#conditional execution: The boolean expression after the if statement. We end the if statement with a colon character and the lines after the if statement are indented.
+#alternative execition: two possibilites and the condition determines which one gets executed. (else)
+#chained conditionals: more than two possibilies and the conditions (elif, else)
