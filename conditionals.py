f74b1ea0f91dba1be24316d0637677329e2a80fd
cmis <cmis@cmis-hs16.(none)>
2016-03-29 09:56:40 +0700

    saved files

--- conditionals.py
+++ conditionals.py
@@ -1,64 +1,34 @@
-#conditional execution: The boolean expression after the if statement. We end the if statement with a colon character and the lines after the if statement are indented.
-#alternative execition: two possibilites and the condition determines which one gets executed. (else)
-#chained conditionals: more than two possibilies and the conditions (elif, else)
 #You are trapped in a cage. You need to find the right key that has enough power to break the door of the cage. However the monster who is guarding you will also use some kind of key to lock you up. If you choose the key that is more powerful that the key the monster uses then you will win. However if the mosnster chooses a key that is more powerful that yours then the monster will win. 
-def playerKeyColor():
+import random 
+def eat(item):
 
-    if color == "red":
-        power = 0    
-    elif color == "orange":
-        power = 1
-    elif color == "yello":
-        power = 2
-    elif color == "green":
-        power = 3
-    elif color == "blue":
-        power = 4
-    elif color == "brown":
-        power = 5
-    elif color == "purple":
-        power = 6
-    elif color == "pink":
-        power = 7
-    elif color == "white":
-        power = 8
-    elif color == "maroon":
-        power = 9
-    elif color == "black":
-        power = 10
+    if item == "red":
+        strength = 0    
+    elif item == "orange":
+        strength = 1
+    elif item == "yellow":
+        strength = 2
+    elif item == "green":
+        strength = 3
+    elif item == "blue":
+        strength = 4
+    elif item == "brown":
+        strength = 5
+    elif item == "purple":
+        strength = 6
+    elif item == "back":
+        strength = 7
+    elif item == "white":
+        strength = 8
+    elif item == "mint":
+        strength = 9
+    elif item == "pink ":
+        strength = 10
     else:
         strength = random.randint(0,10)
     return strength * random.random()
 
-def monsterKeyColor():
-	if color == "red":
-		power = 0    
-	elif color == "orange":
-		power = 1
-	elif color == "yello":
-		power = 2
-	elif color == "green":
-		power = 3
-	elif color == "blue":
-		power = 4
-	elif color == "brown":
-		power = 5
-	elif color == "purple":
-		power = 6
-	elif color == "pink":
-		power = 7
-	elif color == "white":
-		power = 8
-	elif color == "maroon":
-		power = 9
-	elif color == "black":
-		power = 10
-	else:
-		strength = random.randint(0,10)
-	return strength * random.random()
-
-
-def keyPowerValue():
+def getAttackValue():
  
     number = int(raw_input("Type attack value (0-99): "))
     if number > 99 or number < 0:
@@ -66,12 +36,12 @@ def keyPowerValue():
     attackValue = float(number)/100
     return attackValue    
 
-def wrestle(playerKeyPower, monsterKeyPower, keyPowerValue):
+def wrestle(playerKeyPower, monsterKeyPower, playerAttackKeyValue):
 
     targetValue = random.random()
-    playerKeyPower = playerStrength + abs(playerKeyPower - targetValue)    
-    monsterKeyPower =  enemyStrength + targetValue
-    if playerKeyPower > monsterKeyPower:
+    playerAttackResult = playerKeyPower + abs(playerAttackKeyValue - targetValue)    
+    enemyAttackResult =  monsterKeyPower + targetValue
+    if playerAttackResult > enemyAttackResult:
         return True
     else:
         return False
@@ -83,33 +53,34 @@ def resultTemplate(playerKeyPower, monsterKeyPower, result):
     else:
         msg = "loses"
     return """
-Player's key strength: {}
-Monster's key strength: {}
+Player strength: {}
+Enemy strength: {}
 Player {}!
-""".format(playerStrength, enemyStrength, msg)
-
+""".format(playerKeyPower, monsterKeyPower, msg)
 
 def main():
 
 
-    playerKeyColor = raw_input("What colored key are you going to choose? ")
-    monsterKeyColor = raw_input("What colored key is the monster going to choose? ")
-   
+    playerColor = raw_input("What colored key would you like to pick? ")
+    monster1Color = raw_input("What colored key does monster 1 pick? ")
+    monster2Color = raw_input("What colored key does monster 2 pick? ")
 
-playerKeyPower = playerKeyColor
-monsterKeyPower = monsterKeyColor
+    playerKeyPower = color(playerFood)
+    monster1KeyPower = color(enemy1Food)
+    monster2KeyPower = color(enemy2Food)
+
+    playerAttackValue1 = getAttackValue()
+    result1 = wrestle(playerKeyPower, enemy1Strength, playerAttackKeyValue1)
+
+    playerAttackValue2 = getAttackValue()   
+    result2 = wrestle(playerKeyPower, enemy2Strength, playerAttackKeyValue2)
     
-
-playerAttackValue1 = keyPowerValue()
-result1 = wrestle(playerKeyPower, monsterKeyPower, playerAttackValue1)
-
-    
-output = """
-You chose the {} key.
-The monster chose the {} key.
+    output = """
+You picked the {} colored key.
+Monster 1 picked the {} colored key.
+Monster 2 picked the {} colored key.
 """.format(playerFood, enemy1Food, enemy2Food)
-output += resultTemplate(playerStrength, enemy1Strength, result1)
-   
+    output += resultTemplate(playerKeyPower, enemy1Strength, result1)
+    output += resultTemplate(playerKeyPower, enemy2Strength, result2)
 
-print output
-main()
+    print output
