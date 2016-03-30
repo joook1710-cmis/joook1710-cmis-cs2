52a8065aa3af1587cc21c03208906e9020153a6c
cmis <cmis@cmis-hs16.(none)>
2016-03-30 11:15:52 +0700

    saved

--- conditionals.py
+++ conditionals.py
@@ -1,9 +1,9 @@
 #You are trapped in a cage. You need to find the right key that has enough power to break the door of the cage. However the monster who is guarding you will also use some kind of key to lock you up. If you choose the key that is more powerful that the key the monster uses then you will win. However if the mosnster chooses a key that is more powerful that yours then the monster will win. 
 import random 
-def eat(item):
+def key_color(item):
 
     if item == "red":
-        strength = 0    
+        strength = 0  
     elif item == "orange":
         strength = 1
     elif item == "yellow":
@@ -28,20 +28,26 @@ def eat(item):
         strength = random.randint(0,10)
     return strength * random.random()
 
-def getAttackValue():
+def key_shape(item):
+
+	if item == "circle"
+		strength = 5
+	elif item == "triangle" 
+
+def getKickValue():
  
-    number = int(raw_input("Type attack value (0-99): "))
-    if number > 99 or number < 0:
-        number = random.randint(0, 99)
-    attackValue = float(number)/100
-    return attackValue    
+    number = int(raw_input("Type Karate kick power (0-10): "))
+    if number > 10 or number < 0:
+        number = random.randint(0, 10)
+    KickValue = float(number)/100
+    return KickValue    
 
 def wrestle(playerKeyPower, monsterKeyPower, playerAttackKeyValue):
 
     targetValue = random.random()
     playerAttackResult = playerKeyPower + abs(playerAttackKeyValue - targetValue)    
-    enemyAttackResult =  monsterKeyPower + targetValue
-    if playerAttackResult > enemyAttackResult:
+    monsterAttackResult =  monsterKeyPower + targetValue
+    if playerAttackResult > monsterAttackResult:
         return True
     else:
         return False
@@ -49,38 +55,39 @@ def wrestle(playerKeyPower, monsterKeyPower, playerAttackKeyValue):
 def resultTemplate(playerKeyPower, monsterKeyPower, result):
 
     if result == True:
-        msg = "wins"
+        msg = "are free"
     else:
-        msg = "loses"
+        msg = "are still stuck in the cage"
     return """
-Player strength: {}
-Enemy strength: {}
-Player {}!
+Your key power: {}
+Monster's key power: {}
+You {}!
 """.format(playerKeyPower, monsterKeyPower, msg)
 
 def main():
 
 
     playerColor = raw_input("What colored key would you like to pick? ")
-    monster1Color = raw_input("What colored key does monster 1 pick? ")
-    monster2Color = raw_input("What colored key does monster 2 pick? ")
+    monster1Color = raw_input("What colored key does monster Koola pick? ")
+    monster2Color = raw_input("What colored key does monster Kooko pick? ")
 
-    playerKeyPower = color(playerFood)
-    monster1KeyPower = color(enemy1Food)
-    monster2KeyPower = color(enemy2Food)
+    playerKeyPower = key_color(playerColor)
+    monster1KeyPower = key_color(monster1Color)
+    monster2KeyPower = key_color(monster2Color)
 
-    playerAttackValue1 = getAttackValue()
-    result1 = wrestle(playerKeyPower, enemy1Strength, playerAttackKeyValue1)
+    playerAttackValue1 = getKickValue()
+    result1 = wrestle(playerKeyPower, monster1KeyPower, playerAttackValue1)
 
-    playerAttackValue2 = getAttackValue()   
-    result2 = wrestle(playerKeyPower, enemy2Strength, playerAttackKeyValue2)
+    playerAttackValue2 = getKickValue()   
+    result2 = wrestle(playerKeyPower, monster2KeyPower, playerAttackValue2)
     
     output = """
-You picked the {} colored key.
-Monster 1 picked the {} colored key.
-Monster 2 picked the {} colored key.
-""".format(playerFood, enemy1Food, enemy2Food)
-    output += resultTemplate(playerKeyPower, enemy1Strength, result1)
-    output += resultTemplate(playerKeyPower, enemy2Strength, result2)
+You picked the {} key.
+Monster Koola picked the {} key.
+Monster Kooko picked the {} key.
+""".format(playerColor, monster1Color, monster2Color)
+    output += resultTemplate(playerKeyPower, monster1KeyPower, result1)
+    output += resultTemplate(playerKeyPower, monster2KeyPower, result2)
 
     print output
+main()
