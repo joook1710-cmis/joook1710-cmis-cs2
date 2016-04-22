a6cd93f32ee5298cdd88c63749e5858fed606348
cmis <cmis@cmis-hs16.(none)>
2016-04-22 09:54:11 +0700

    saved

--- countdown.py
+++ countdown.py
@@ -1,12 +1,50 @@
 def countup(n):
-	if n >= 6656:
-		print "Blastoff!"
+	if n >= 10:
+		print "Ok!"
 	else:
 		print n
 		countup (n+1)
+def countdown(n):
+	if n <= 0:
+		print "Blastoff!"
+	else:
+		print n
+		countdown(n-1)
+
+def countup(start, stop):
+	if start > stop:
+		print "Countup done!"
+	else:
+		print start
+		countup(start + 1, stop)
+
+def countdown(start, stop):
+	if start < stop:
+		print "Countdown done!"
+	else:
+		print start
+		countdown(start - 1, stop)
+
+
+def adder(stuff, total):
+    if stuff == "":
+        return total
+    else:
+        total += float(stuff) 
+        stuff = raw_input("Running total: " + str(total) + "\nNext number: ")
+        return adder(stuff, total)
 
 def main():
-	countup(1)
+    out = adder(0, 0)
+    print "The sum is " + str(out)
+main()
+
+
+def main():
+	countup(1, 7)
+	countdown(7, 1)
 	return
 main()
+
+
 	
