fdf910ed590ebbe201e5b4448c9712d4e525b262
cmis <cmis@cmis-hs16.(none)>
2016-04-29 11:48:32 +0700

    saved

--- countdown.py
+++ countdown.py
@@ -1,67 +1,84 @@
-def countup(n):
-	if n >= 10:
-		print "Ok!"
-	else:
-		print n
-		countup (n+1)
-def countdown(n):
-	if n <= 0:
-		print "Blastoff!"
-	else:
-		print n
-		countdown(n-1)
+#def countup(n):
+#	if n >= 10:
+#		print "Ok!"
+#	else:
+#		print n
+#		countup (n+1)
+#def countdown(n):
+#	if n <= 0:
+#		print "Blastoff!"
+#	else:
+#		print n
+#		countdown(n-1)
+#
+#def countup(start, stop):
+#	if start > stop:
+#		print "Countup done!"
+#	else:
+#		print start
+#		countup(start + 1, stop)
+#
+#def countdown(start, stop):
+#	if start < stop:
+#		print "Countdown done!"
+#	else:
+#		print start
+#		countdown(start - 1, stop)
+#
+#
+#def adder(stuff, total):
+#    if stuff == "":
+#        return total
+#    else:
+#        total += float(stuff) 
+#        stuff = raw_input("Running total: " + str(total) + "\nNext number: ")
+#        return adder(stuff, total)
 
-def countup(start, stop):
-	if start > stop:
-		print "Countup done!"
-	else:
-		print start
-		countup(start + 1, stop)
-
-def countdown(start, stop):
-	if start < stop:
-		print "Countdown done!"
-	else:
-		print start
-		countdown(start - 1, stop)
+#def main():
+#    out = adder(0, 0)
+#    print "The sum is " + str(out)
+#main()
 
 
-def adder(stuff, total):
-    if stuff == "":
-        return total
-    else:
-        total += float(stuff) 
-        stuff = raw_input("Running total: " + str(total) + "\nNext number: ")
-        return adder(stuff, total)
+#def main():
+#	countup(1, 7)
+#	countdown(7, 1)
+#	return
+#main()
 
-def main():
-    out = adder(0, 0)
-    print "The sum is " + str(out)
-main()
-
-
-def main():
-	countup(1, 7)
-	countdown(7, 1)
-	return
-main()
-
-def biggest(n1, largest):
-	if n1 == "":
-		return largest 
-	else:
-		largest = float(n1) 
-		n1 = raw_input("\nNext: ")
-	if largest >
-		return biggest( n1, largest)
-
-def main():
-	output = biggest(0, 0)
-
-	print "The biggest number is " + str(output)
-main()
 	
+#def biggest(lastnumber):
+#    number = raw_input("Next number: ")
+#    if number == "":
+#        return lastnumber
+#    elif lastnumber < float(number):
+#        lastnumber = float(number)
+#       return biggest(lastnumber)
+#    else:
+#        return biggest(lastnumber)
 
+
+#def main():
+#    out = biggest(-float('Inf'))
+#    print "The biggest number is " + str(out)
+#main()
+
+#def smallest(lastnumber):
+#	number = raw_input("Next number:")
+#	if number == "":
+#		return lastnumber
+#	elif lastnumber > float(number):
+#		lastnumber = float(number)
+#		return smallest(lastnumber)
+#	else:
+#		return smallest(lastnumber)
+
+#def main():
+#	out = smallest(float('Inf'))
+#	print "The smallest number is " + str(out)
+#main()
+
+def pow(x, n):
 
 
 	
