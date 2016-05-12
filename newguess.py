f6eb64318a1513deeb6e46c4ea6eff022d26bcfd
cmis <cmis@cmis-hs16.(none)>
2016-05-12 11:52:45 +0700

    yo

--- newguess.py
+++ newguess.py
@@ -1,1 +1,42 @@
-def game()
+def game():
+
+	if guess > targetnumber:
+		print "The number is too low."
+
+	elif guess < targetnumber:
+		print "The number is too high."
+
+	elif guess == targetnumber:
+		print "You guessed the right number!"
+
+
+
+def main():
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
