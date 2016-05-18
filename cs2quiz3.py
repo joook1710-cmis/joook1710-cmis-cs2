0415305c1ef91f084f38e43e362df3359d358199
cmis <cmis@cmis-hs16.(none)>
2016-05-18 11:41:27 +0700

    Test

---
+++ cs2quiz3.py
@@ -0,0 +1,60 @@
+# Recursive Function makes a function repeat itself.
+# The resursive function would not stop.
+# The base case and the recursive case.
+# By using ":". You have to tab everything in as well.
+# By using "return" or "print". 
+
+#a1 = 2
+#a2 = 6
+#a3 = -2
+
+#b1 = 8
+#b2 = 2
+#b3 = -2
+
+#c1 = -2
+#c2 = 3
+#c3 = 5
+
+#d1 = -4
+#d2 = 8
+#d3 = -4
+
+import math 
+
+def odd_numbers(numbers):
+    total = 0
+if numbers 2 == 1:
+            total += num
+print total
+
+def odd_numbers(numbers):
+    odd = []
+if numbers 2 == 1:
+            odd.append(num)
+print sum(odd)
+
+def odd_numbers(numbers):
+    print sum(num for num in numbers if num 2 == 1)
+
+def odd_numbers(chosen_number, estimation):
+	guess_correct = """
+The average of your odd numbers was {}.
+""".format(chosen_number, estimation)
+	return odd_numbers
+
+
+def main():
+#base case
+	if odd_numbers == "":
+		print "Adding the odd numbers!"
+#recursive case
+	else: 
+	numbers = raw_input("\nNext: ")
+
+	
+
+main()
+
+
+
