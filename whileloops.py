319d37fffb4dd45136708609b81ff8b717a97b00
cmis <cmis@cmis-hs16.(none)>
2016-05-24 09:53:00 +0700

    saved

---
+++ whileloops.py
@@ -0,0 +1,18 @@
+def countdown(x):
+    while x > -1:
+        print x
+        x -= 1
+
+countdown(10)
+countdown(20)
+
+def counter(x):
+    while x>0: 
+        print x
+        x -= 1
+    while x<0:
+        print 
+        x += 1
+
+counter(10)
+counter(-3)
