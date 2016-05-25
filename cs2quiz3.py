9414d3be1687ec599b6978b6ebcfc0ec3a35f1d6
cmis <cmis@cmis-hs07.(none)>
2016-05-25 11:16:35 +0700

    saved

--- cs2quiz3.py
+++ cs2quiz3.py
@@ -1,60 +1,38 @@
-# Recursive Function makes a function repeat itself.
-# The resursive function would not stop.
-# The base case and the recursive case.
-# By using ":". You have to tab everything in as well.
-# By using "return" or "print". 
+# Recursive Function makes a function repeat itself. (+1)
+# The resursive function would not stop. (+1)
+# The base case and the recursive case. (+1)
+# By using ":". You have to tab everything in as well. (-1)
+# By using "return" or "print". (-1)
 
-#a1 = 2
-#a2 = 6
-#a3 = -2
+#a1 = 8 (1)
+#a2 = 8 (1)
+#a3 = -1 (1)
 
-#b1 = 8
-#b2 = 2
-#b3 = -2
+#b1 = 2 (1)
+#b2 = 2 (1)
+#b3 = 4 (1)
 
-#c1 = -2
-#c2 = 3
-#c3 = 5
+#c1 = -2 (1)
+#c2 = 4 (1)
+#c3 = 45 (1)
 
-#d1 = -4
-#d2 = 8
-#d3 = -4
+#d1 = 6 (1)
+#d2 = 8 (1)
+#d3 = 4 (1)
 
-import math 
-
-def odd_numbers(numbers):
-    total = 0
-if numbers 2 == 1:
-            total += num
-print total
-
-def odd_numbers(numbers):
-    odd = []
-if numbers 2 == 1:
-            odd.append(num)
-print sum(odd)
-
-def odd_numbers(numbers):
-    print sum(num for num in numbers if num 2 == 1)
-
-def odd_numbers(chosen_number, estimation):
-	guess_correct = """
-The average of your odd numbers was {}.
-""".format(chosen_number, estimation)
-	return odd_numbers
-
-
+def average_odd_numbers(sum, ct):
+    n = raw_input("Next: ")
+    if n == "":
+        #BASE CASE
+        return sum/ct
+    else:
+        #RECURSIVE CASE
+        n = float(n)
+        if n % 2 == 1:
+            sum += n
+            ct += 1
+        return average(sum, ct)
 def main():
-#base case
-	if odd_numbers == "":
-		print "Adding the odd numbers!"
-#recursive case
-	else: 
-	numbers = raw_input("\nNext: ")
-
-	
-
+    a = average(0.0 ,0.0)
+    print "The average of your odd numbers was {}.".format(a)
 main()
-
-
-
